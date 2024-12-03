d00a040676084b98cd948a878d9c483b4bddc960
gvelasquezneira <gvelasquezneira@ufl.edu>
2024-12-03 12:18:08 -0500

    commit31

--- js/countyDepartments.js
+++ js/countyDepartments.js
@@ -3,16 +3,16 @@ let departmentData = {};
 fetch('js/countyDepartments.json')
     .then(response => response.json())
     .then(data => {
-        departmentData = data; 
+        departmentData = data;
         populatedepartmentDropdown();
-    })
+    });
 
 function populatedepartmentDropdown() {
     const dropdown = document.getElementById('departmentDropdown');
     
-    for (const department in departmentData) {
+    for (const department in departmentData.department) {
         const li = document.createElement('li');
-        li.innerHTML = `<a class='dropdown-item' href='#' onclick='loaddepartment("${department}")'>${departmentData[department].name}</a>`;
+        li.innerHTML = `<a class='dropdown-item' href='#' onclick='loaddepartment("${department}")'>${department}</a>`;
         dropdown.appendChild(li);
     }
 }
@@ -23,181 +23,34 @@ function loaddepartment(departmentId) {
 
     const contentArea = document.getElementById('departmentContent');
     const welcomeSection = document.getElementById('welcomeSection');
-    contentArea.innerHTML = '';
+    contentArea.innerHTML = ''; 
 
-    contentArea.innerHTML += `<div class="col-12 mb-4"><h2>${county.name}</h2></div>`;
+   
+    contentArea.innerHTML += `<div class="col-12 mb-4"><h2>${departmentId}</h2></div>`;
 
-    county.websites.forEach(site => {
+   
+    department.forEach(site => {
         let buttons = '';
+
 
         if (site.urls) {
             const urlsArray = Array.isArray(site.urls) ? site.urls : [site.urls];
-        
+
             urlsArray.forEach((url, index) => {
-                let buttonText = '';
-        
-                if (site.name === "County Commission") {
-                    switch (index) {
-                        case 0:
-                            buttonText = 'Alachua';
-                            break;
-                        case 1:
-                            buttonText = 'Bradoford';
-                            break;
-                        case 2:
-                            buttonText = 'Gilchrist';
-                            break;
-                        case 3:
-                            buttonText = 'Levy';
-                            break;
-                        case 4:
-                            buttonText = 'Union';
-                            break;
-                        default:
-                            buttonText = `Visit Website`;
-                    }
-                }
-        
-                else if (site.name === "Sheriff's Office") {
-                    switch (index) {
-                        case 0:
-                            buttonText = 'Alachua';
-                            break;
-                        case 1:
-                            buttonText = 'Bradoford';
-                            break;
-                        case 2:
-                            buttonText = 'Gilchrist';
-                            break;
-                        case 3:
-                            buttonText = 'Levy';
-                            break;
-                        case 4:
-                            buttonText = 'Union';
-                            break;
-                        default:
-                            buttonText = `Visit Website`;
-                    }
-                }
-                else if (site.name === "Property Appraiser") {
-                    switch (index) {
-                        case 0:
-                            buttonText = 'Alachua';
-                            break;
-                        case 1:
-                            buttonText = 'Bradoford';
-                            break;
-                        case 2:
-                            buttonText = 'Gilchrist';
-                            break;
-                        case 3:
-                            buttonText = 'Levy';
-                            break;
-                        case 4:
-                            buttonText = 'Union';
-                            break;
-                        default:
-                            buttonText = `Visit Website`;
-                    }
-                }
-                else if (site.name === "School Board") {
-                    switch (index) {
-                        case 0:
-                            buttonText = 'Alachua';
-                            break;
-                        case 1:
-                            buttonText = 'Bradoford';
-                            break;
-                        case 2:
-                            buttonText = 'Gilchrist';
-                            break;
-                        case 3:
-                            buttonText = 'Levy';
-                            break;
-                        case 4:
-                            buttonText = 'Union';
-                            break;
-                        default:
-                            buttonText = `Visit Website`;
-                    }
-                }
-                else if (site.name === "Clerk of Court") {
-                    switch (index) {
-                        case 0:
-                            buttonText = 'Alachua';
-                            break;
-                        case 1:
-                            buttonText = 'Bradoford';
-                            break;
-                        case 2:
-                            buttonText = 'Gilchrist';
-                            break;
-                        case 3:
-                            buttonText = 'Levy';
-                            break;
-                        case 4:
-                            buttonText = 'Union';
-                            break;
-                        default:
-                            buttonText = `Visit Website`;
-                    }
-                }
-        
-                else if (site.name === "Tax Collector") {
-                    switch (index) {
-                        case 0:
-                            buttonText = 'Alachua';
-                            break;
-                        case 1:
-                            buttonText = 'Bradoford';
-                            break;
-                        case 2:
-                            buttonText = 'Gilchrist';
-                            break;
-                        case 3:
-                            buttonText = 'Levy';
-                            break;
-                        case 4:
-                            buttonText = 'Union';
-                            break;
-                        default:
-                            buttonText = `Visit Website`;
-                    }
-                }
-                else if (site.name === "Jail") {
-                    switch (index) {
-                        case 0:
-                            buttonText = 'Alachua';
-                            break;
-                        case 1:
-                            buttonText = 'Bradoford';
-                            break;
-                        case 2:
-                            buttonText = 'Gilchrist';
-                            break;
-                        case 3:
-                            buttonText = 'Levy';
-                            break;
-                        case 4:
-                            buttonText = 'Union';
-                            break;
-                        default:
-                            buttonText = `Visit Website`;
-                    }
-                }
-                else {
-                    buttonText = "Visit Website";
-                }
-        
+                
+                let buttonText = `Visit Website`; 
+                
+                const countyNames = ['Alachua', 'Bradford', 'Gilchrist', 'Levy', 'Union'];
+                buttonText = countyNames[index] || buttonText; 
+
+               
                 buttons += `
-    <div class="d-flex flex-row align-items-center justify-content-between mb-2">
-        <a href="${url}" class="btn btn-primary me-2 align-text-bottom" target="_blank">${buttonText}</a>
-    </div>
-`;
+                    <div class="d-flex flex-row align-items-center justify-content-between mb-2">
+                        <a href="${url}" class="btn btn-primary me-2 align-text-bottom" target="_blank">${buttonText}</a>
+                    </div>
+                `;
             });
         }
-        
-              
 
         contentArea.innerHTML += `
             <div class="col-md-6 mb-4">
@@ -212,11 +65,8 @@ function loaddepartment(departmentId) {
                 </div>
             </div>
         `;
-
     });
 
     welcomeSection.style.display = 'none';
     contentArea.style.display = 'flex';
 }
-
-    
