1300efd947189948241879681554a5c0895fd608
gvelasquezneira <gvelasquezneira@ufl.edu>
2024-12-03 09:57:56 -0500

    commit14

--- js/county.js
+++ js/county.js
@@ -364,6 +364,88 @@ function loadCounty(countyId) {
                         buttonText = "Inmate Search";
                     } 
                 }
+                else if (site.name === "Gilchrist County Commission") {
+                    switch (index) {
+                        case 0:
+                            buttonText = 'County';
+                            break;
+                        case 1:
+                            buttonText = 'Board';
+                            break;
+                        case 2:
+                            buttonText = 'Meetings';
+                            break;
+                        case 3:
+                            buttonText = 'PRR';
+                            break;
+                        default:
+                            buttonText = `Visit Website`;
+                    }
+                }
+                
+                else if (site.name === "Gilchrist Sheriff's Office") {
+                    switch (index) {
+                        case 0:
+                            buttonText = 'Sheriff';
+                            break;
+                        case 1:
+                            buttonText = 'PRR';
+                            break;
+                        default:
+                            buttonText = `Visit Website`;
+                    }
+                }
+                else if (site.name === "Gilchrist Property Appraiser") {
+                    switch (index) {
+                        case 0:
+                            buttonText = 'Property Appraiser';
+                            break;
+                        case 1:
+                            buttonText = 'Property Search';
+                            break;
+                        case 2:
+                            buttonText = 'Map';
+                            break;
+                        default:
+                            buttonText = `Visit Website`;
+                    }
+                }
+                else if (site.name === "Gilchrist School Board") {
+                    switch (index) {
+                        case 0:
+                            buttonText = 'School Board';
+                            break;
+                        case 1:
+                            buttonText = 'Board';
+                            break;
+                        case 2:
+                            buttonText = 'Superintendent';
+                            break;
+                        case 3:
+                            buttonText = 'Finances';
+                            break;
+                        case 4:
+                            buttonText = 'Directory';
+                            break;
+                        case 5:
+                            buttonText = 'PRR';
+                            break;
+                        case 6:
+                            buttonText = 'Meetings';
+                            break;
+                        default:
+                            buttonText = `Visit Website`;
+                    }
+                }
+                else if (site.url) {
+                    if (site.name === "Gilchrist Clerk of Court") {
+                        buttonText = "Clerk of Court";
+                    } else if (site.name === "Gilchrist Tax Collector") {
+                        buttonText = "Tax Collector";
+                    } else if (site.name === "Gilchrist Jail") {
+                        buttonText = "Inmate Search";
+                    } 
+                }
                 else {
                     buttonText = "Visit Website";
                 }
